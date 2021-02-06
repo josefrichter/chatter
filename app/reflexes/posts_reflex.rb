3a6675d258defa23851bd57a014fa4fd56cd98ec
Josef Richter <josef.richter@wilmersrespondek.com>
2021-02-06 18:26:24 +0100

    fixed broadcasting of likes and reposts counts

--- app/reflexes/posts_reflex.rb
+++ app/reflexes/posts_reflex.rb
@@ -2,24 +2,25 @@
 
 class PostsReflex < ApplicationReflex
 
+
   def repost
     post = Post.find(element.dataset[:id])
     post.increment! :reposts_count
     cable_ready["timeline"].text_content(
-      selector: "#post-${post.id}-reposts",
+      selector: "#post-#{post.id}-reposts",
       text: post.reposts_count
     )
-    cable_ready.broadcast
+    cable_ready["timeline"].broadcast
   end
 
   def like
     post = Post.find(element.dataset[:id])
     post.increment! :likes_count
     cable_ready["timeline"].text_content(
-      selector: "#post-${post.id}-likes",
+      selector: "#post-#{post.id}-likes",
       text: post.likes_count
     )
-    cable_ready.broadcast
+    cable_ready["timeline"].broadcast
   end
 
   # Add Reflex methods in this file.
